5ab73a7ff2c12726879ae00b4e97359f21cc04e8
joannawinchester <joannawinchester@gmail.com>
2016-01-03 16:27:16 -0500

    updated database migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160103182054) do
+ActiveRecord::Schema.define(version: 20160103212224) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20160103182054) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "price"
+    t.string   "price"
     t.string   "subtitle"
     t.string   "note"
   end
