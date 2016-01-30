2f26a0dd965a6e06c0711b0a7139813591d1052f
joannawinchester <joannawinchester@gmail.com>
2016-01-30 14:09:31 -0500

    Add carousel to landing-page, add partial for products

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,11 @@
 class StaticPagesController < ApplicationController
   
   def index
+  	@products = Product.limit(3)
   end
 
 	def landing_page
-		@featured_product = Product.first
+		@products = Product.limit(3)
 	end
 	
 end
