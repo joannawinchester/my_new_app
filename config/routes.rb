c1761884795b7bd9218e5b7ec9361118b37a0bc3
joannawinchester <joannawinchester@gmail.com>
2016-02-07 09:33:47 -0500

    Reroute root page; use redirects in product create, update, destroy; make nav links dynamic; create dynamic links in product index to show views

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
 Rails.application.routes.draw do
+
+  root 'static_pages#about'
+
   resources :products
+
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
   get 'static_pages/index'
 
-  get 'static_pages/products'
+  get 'static_pages/landing_page'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#landing_page'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
