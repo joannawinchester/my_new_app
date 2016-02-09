35657209f3abe81b8a9685741fd2fe15e3ddb874
joannawinchester <joannawinchester@gmail.com>
2016-02-09 08:34:20 -0500

    add contact form and mailer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  ActionMailer::Base.mail(:from => 'from@domain.com', :to => 'to@domain.com', :subject => "Welcome to My Awesome Site", :body => 'I am the email body.').deliver_now
+
+  def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+  	ActionMailer::Base.mail(:from => @email, 
+  		:to => 'stitchphilly@gmail.com',
+  		:subject => "A new contact form message from #{@name}",
+  		:body => @message).deliver_now
+  end
+
 end
