9d7876749aaa6cb0a0f0392ed57261d280ababa4
joannawinchester <joannawinchester@gmail.com>
2016-02-18 07:08:29 -0500

    restrict delete and destroy actions to admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,7 @@ class Ability
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
     if user.admin?
-    	can :destroy, Comment
+    	can [:destroy, :delete], Comment
     else
     	can :read, Comment
     end
