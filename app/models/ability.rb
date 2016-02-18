3f29228713c6b69f783d208ea91eab89b554dae1
joannawinchester <joannawinchester@gmail.com>
2016-02-18 07:43:53 -0500

    update admin access

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,7 @@ class Ability
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
     if user.admin?
-    	can [:destroy, :delete], Comment
+    	can [:manage], all
     else
     	can :read, Comment
     end
