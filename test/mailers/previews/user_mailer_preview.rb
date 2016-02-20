c12d89d4e367b6f2ed08992dc3155447ede5a5a7
joannawinchester <joannawinchester@gmail.com>
2016-02-20 08:33:56 -0500

    add rspec to project

--- test/mailers/previews/user_mailer_preview.rb
+++
@@ -1,8 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
-class UserMailerPreview < ActionMailer::Preview
-
-	def contact_form
-		UserMailer.contact_form("joannawinchester@gmail.com", "Joanna", "Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...Some test filler...")
-	end
-
-end
