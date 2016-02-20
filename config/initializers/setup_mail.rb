a13b49cb23b7c84aa78a5619fe9244137dfc4f41
joannawinchester <joannawinchester@gmail.com>
2016-02-19 19:41:57 -0500

    Updated email password

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,
   :domain               => 'gmail.com',
   :user_name            => 'joannawinchester@gmail.com',
-  :password             => 'CherylCharlieOscar',
+  :password             => 'BaltimoreLarchwood',
   :authentication       => 'plain',
   :enable_starttls_auto => true
   }
