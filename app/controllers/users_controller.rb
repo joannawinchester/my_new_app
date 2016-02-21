5876622cad14f9d617ae243d0b80b699bb0e11d0
joannawinchester <joannawinchester@gmail.com>
2016-02-20 19:13:42 -0500

    remove byebug from users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,6 @@ class UsersController < ApplicationController
 
   # GET /users/new
   def new
-    byebug
     @user = User.new
   end
 
@@ -29,7 +28,6 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    byebug
     @user = User.new(user_params)
 
     respond_to do |format|
